2029397c9229a50add4cfe633dc847236c26a088
Carol A. Nikolic <carolnikolic@gmail.com>
2023-12-29 21:16:28 +0100

    refactor: change name of data for better description.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-  res.render("index.ejs",  { recipeJson: [] });
+  res.render("index.ejs",  { recipe: [] });
 });
 
 
